ebcd2781bad2744dc8dea912f110e49cb268762d
Rebeca Pacheco <rbkgarner@gmail.com>
2020-01-04 13:09:05 -0300

    function Tabnav and init accordion

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
+/** Tab animais */
+function Tabnav() {
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const tabcontent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-    tabcontent.forEach((section) => {
-        section.classList.remove('active');
+    function activeTab(index) {
+        tabcontent.forEach((section) => {
+            section.classList.remove('active');
+        });
+        tabcontent[index].classList.add('active');
+
+    }
+
+    tabmenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        })
     });
-    tabcontent[index].classList.add('active');
-
 }
-
-tabmenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    } )
-});
+Tabnav();
